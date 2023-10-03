0664d9ce991f99e378aae89b053af96c7cb69909
mdyasin111 <mdyasin7636@gmail.com>
2023-10-03 23:29:04 +0600

    navbar added

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,7 +1,35 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
   return (
-    <div>
-      <p>this is navbar</p>
+    <div className="bg-white">
+     <nav className="flex justify-between items-center w-[92%] mx-auto">
+      <div>
+        <img className="w-20" src="https://i.ibb.co/g9rCyYj/download.png" alt="" />
+      </div>
+      <div className="">
+        <ul className="flex items-center gap-12">
+          <li>
+            <Link>HOME</Link>
+          </li>
+          <li>
+            <Link>SERVICES</Link>
+          </li>
+          <li>
+            <Link>PROJECTS</Link>
+          </li>
+          <li>
+            <Link>BLOGS</Link>
+          </li>
+          <li>
+            <Link>ABOUT US</Link>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <button className="text-white px-4 py-2 rounded-md bg-black">CONTACT US</button>
+      </div>
+     </nav>
     </div>
   );
 };
