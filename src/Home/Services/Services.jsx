a445bbe321bd19a199dd727b0488ecbb205eaf5a
mdyasin111 <mdyasin7636@gmail.com>
2023-10-15 19:16:44 +0600

    slider updated

--- src/Home/Services/Services.jsx
+++ src/Home/Services/Services.jsx
@@ -3,6 +3,7 @@ const Services = () => {
     return (
         <div>
              <p className="text-3xl text-blue-800 text-center my-5">Services Section</p>
+             
         </div>
     );
 };
