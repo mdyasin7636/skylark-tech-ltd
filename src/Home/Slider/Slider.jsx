7386744c28b86098133b864fab4780e07b307213
mdyasin111 <mdyasin7636@gmail.com>
2023-10-16 12:44:12 +0600

    slider updated

--- src/Home/Slider/Slider.jsx
+++ src/Home/Slider/Slider.jsx
@@ -1,10 +1,33 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
 
 const Slider = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div >
+      <Swiper
+        rewind={true}
+        navigation={true}
+        modules={[Navigation]}
+        className="mySwiper"
+        style={{
+          "--swiper-navigation-color": "#fff",
+          "--swiper-pagination-color": "#fff",
+        }}
+      >
+        <SwiperSlide>
+          <img src="https://i.ibb.co/pXGBP7P/img-1.jpg" alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://i.ibb.co/TM6C4Gr/img-2.jpg" alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://i.ibb.co/QpxMX4S/img-3.jpg" alt="" />
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
 };
 
 export default Slider;
--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className="shadow-md w-full fixed top-0 left-0">
+    <div className="shadow-md w-full top-0 left-0">
       <div className="md:px-10 py-4 px-7 md:flex justify-between items-center bg-white">
         {/* logo here */}
         <div className="flex items-center gap-1 text-2xl cursor-pointer">
@@ -27,16 +27,16 @@ const Navbar = () => {
 
         <div
           onClick={() => setIsOpen(!isOpen)}
-          className="absolute right-8 top-6 cursor-pointer md:hidden"
+          className="absolute right-8 top-6 cursor-pointer md:hidden z-10"
         >
           {isOpen ? <AiOutlineClose /> : <FiMenu />}
         </div>
 
         {/* nav links */}
         <ul
-          className={`md:flex md:items-center md:pb-0 pb-8 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-300 ease-in ${
+          className={`md:flex md:items-center md:pb-0 pb-8 absolute md:static bg-white left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-300 ease-in ${
             isOpen ? "top-12" : "top-[-490px]"
-          }`}
+          } z-10`}
         >
           {Links.map((link, index) => (
             <li key={index} className="font-semibold my-7 md:my-0 md:ml-8">
